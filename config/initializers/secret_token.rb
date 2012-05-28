e375e34c0456d71e86d290597d5cf2b2aca1cd74
Holger Schemion <holger@schemion.de>
2012-05-29 00:51:24 +0200

    Mein erster Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColesRails::Application.config.secret_token = '3b7a54269cfe38a0aaa3756c99638c4349e924dea39bf0b9a310180fd578b3a322b3d09385933f1c9e963f4b7560ad02526ad8100a245109354cf85ecf36d9cd'
